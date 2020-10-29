47c1276c95dc60a3200f48c835e1ebbe4a203564
HeatherAckerman <hla20115213@gmail.com>
2020-10-29 16:23:12 -0500

    Created burgers_db and burgers table

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	burgerName VARCHAR(15) NOT NULL,
+	devoured BOOLEAN
+);
